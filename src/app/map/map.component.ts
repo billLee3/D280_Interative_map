6ba1be254331a27c1d59e9e4991055735128fed3
William Lee <wlee90@wgu.edu>
2023-06-05 07:08:42 -0500

    Media queries

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./map.component.css']
 })
 export class MapComponent {
-
+  // Event listener for a click. If clicked then you would highlight a different color
+  // Send the ISO code to the API call
+  // Transmit the API information into the right column
 }
